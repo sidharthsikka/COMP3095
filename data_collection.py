e32175ae4a048cc6508332f176baff7bf4c93623
Sidharth Sikka <sidharthsikka@gmail.com>
2018-02-18 10:45:30 +0000

    Initial commit

---
+++ data_collection.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+filenames = ['WIOD_Data/WIOT2000_Nov16_ROW.xlsx','WIOD_Data/WIOT2001_Nov16_ROW.xlsx','WIOD_Data/WIOT2002_Nov16_ROW.xlsx','WIOD_Data/WIOT2003_Nov16_ROW.xlsx','WIOD_Data/WIOT2004_Nov16_ROW.xlsx','WIOD_Data/WIOT2005_Nov16_ROW.xlsx', 'WIOD_Data/WIOT2006_Nov16_ROW.xlsx','WIOD_Data/WIOT2007_Nov16_ROW.xlsx','WIOD_Data/WIOT2008_Nov16_ROW.xlsx','WIOD_Data/WIOT2009_Nov16_ROW.xlsx','WIOD_Data/WIOT2010_Nov16_ROW.xlsx','WIOD_Data/WIOT2011_Nov16_ROW.xlsx','WIOD_Data/WIOT2012_Nov16_ROW.xlsx','WIOD_Data/WIOT2013_Nov16_ROW.xlsx','WIOD_Data/WIOT2014_Nov16_ROW.xlsx']
+
+output = list(range(2000,2015))
+
+for i in range(0, len(filenames)):
+    df = pd.read_excel(filenames[i], skiprows=[1,2],index_col=0, parse_cols=([0] + list(range(3,2467))))
+    df.to_pickle('WIOD_Data/pickled_data/' + str(output[i]) + '.pkl')
+    print(str(output[i]) + " DONE")
+
---
+++ income_expenditure_model.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+df_2000 = pd.read_pickle('WIOD_Data/pickled_data/2000.pkl')
+
+for row in df_2000.iterrows():
+    index, data = row
+    for col in df_2000.columns:
+        if index == col:
+            df_2000.loc[index,col] = 0
+
+G = nx.DiGraph()
+for i in df_2000.columns.values:
+    G.add_node(i)
+
+weight_list = []
+
+for i in df_2000.columns.values:
+    for j in df_2000.columns.values:
+        if i != j and df_2000.loc[i,j]>0:
+            weight_list.append((i,j,df_2000.loc[i,j]))
+
+G.add_weighted_edges_from(weight_list)
+print("added")
+# nx.draw_spring(G)
+# print("DRAWN")
+# plt.savefig('2000_SPRING.png')
+
+
+#Model
+in_strength = [] #in strength of a node which is calculated for all the money coming into it
+for row in df_2000.iterrows():
+    index, data = row
+    in_strength.append(sum(data.tolist()))
+out_strength =[] #out strength of a node which is calculated for all the money going out of a node
+for col in df_2000.columns:
+    out_strength.append(sum(col.values()))
+alpha = [] #combines i and o to give a propensity to spend for a sector by putting o/i if i>o else 1
+for i in range(len(df_2000.columns)):
+    if(in_strength[i] > out_strength[i]):
+        alpha.append(in_strength[i]/out_strength[i])
+    else:
+        alpha.append(1)
+beta = [] #again combines i and o to give a borrowing capacity of a company by putting o-i if o>i else 0
+for i in range(len(df_2000.columns)):
+    if(out_strength[i] > in_strength[i]):
+        beta.append(out_strength[i] - in_strength[i])
+    else:
+        beta.append(0)
+m = [] #for each connection i-j we normalize it by dividing the money going out to j by the total money going out
+y = df_2000.columns
+for i in range(len(out_strength)):
+    m.append(df_2000[y[i]]/out_strength[i])
+M = np.matmul(np.diag(out_strength),m) #diagonal matrix of o multiply m
+one = [1 for _ in range(len(df_2000.columns))]
+E = np.matmul(np.matmul(np.diag(np.transpose(alpha)), np.transpose(M)),np.transpose(one)) + beta
+M_T = []
+M_T.append(M)
+M_1 = [] #Depending on the kind of shock you have to populate this and pass it into M_T, calculate E1 and pass it to E_T and then iterate with the below for loop
+E_T = []
+E_T.append(E)
+for i in range(10):
+    M_T.append(np.matmul(np.diag(E_T[i]),m))
+    E_T.append(np.matmul(np.matmul(np.diag(np.transpose(alpha)), np.transpose(M)),np.transpose(one)) + beta)
+    rms = sqrt(mean_squared_error(E_T[i], E_T[i+1]))
+    if rms == 0:
+        print("CONVERGED")
