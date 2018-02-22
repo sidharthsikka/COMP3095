6dfbdd4261e6ea7fc3dd77fd72c36194b3ccb86b
Sidharth Sikka <sidharthsikka@gmail.com>
2018-02-22 23:38:26 +0000

    M and E NaN

--- income_expenditure_model.py
+++ income_expenditure_model.py
@@ -7,11 +7,18 @@ from math import sqrt
 
 df_2000 = pd.read_pickle('WIOD_Data/pickled_data/2000.pkl')
 
+# Delete any self pointing edges
 for row in df_2000.iterrows():
     index, data = row
     for col in df_2000.columns:
         if index == col:
             df_2000.loc[index,col] = 0
+
+# Delete any country where there is no trade
+    for col in df_2000.columns:
+        if df_2000[col].sum() == 0 and sum(df_2000.loc[col].tolist()) == 0:
+            df_2000.drop(col, axis=1)
+            df_2000.drop(col, axis=0)
 
 G = nx.DiGraph()
 for i in df_2000.columns.values:
@@ -25,7 +32,6 @@ for i in df_2000.columns.values:
             weight_list.append((i,j,df_2000.loc[i,j]))
 
 G.add_weighted_edges_from(weight_list)
-print("added")
 # nx.draw_spring(G)
 # print("DRAWN")
 # plt.savefig('2000_SPRING.png')
@@ -38,11 +44,14 @@ for row in df_2000.iterrows():
     in_strength.append(sum(data.tolist()))
 out_strength =[] #out strength of a node which is calculated for all the money going out of a node
 for col in df_2000.columns:
-    out_strength.append(sum(col.values()))
+    out_strength.append(df_2000[col].sum())
 alpha = [] #combines i and o to give a propensity to spend for a sector by putting o/i if i>o else 1
 for i in range(len(df_2000.columns)):
     if(in_strength[i] > out_strength[i]):
-        alpha.append(in_strength[i]/out_strength[i])
+        if out_strength[i] != 0:
+            alpha.append(in_strength[i]/out_strength[i])
+        else:
+            alpha.append((in_strength[i]))
     else:
         alpha.append(1)
 beta = [] #again combines i and o to give a borrowing capacity of a company by putting o-i if o>i else 0
@@ -58,14 +67,38 @@ for i in range(len(out_strength)):
 M = np.matmul(np.diag(out_strength),m) #diagonal matrix of o multiply m
 one = [1 for _ in range(len(df_2000.columns))]
 E = np.matmul(np.matmul(np.diag(np.transpose(alpha)), np.transpose(M)),np.transpose(one)) + beta
-M_T = []
-M_T.append(M)
-M_1 = [] #Depending on the kind of shock you have to populate this and pass it into M_T, calculate E1 and pass it to E_T and then iterate with the below for loop
-E_T = []
-E_T.append(E)
-for i in range(10):
-    M_T.append(np.matmul(np.diag(E_T[i]),m))
-    E_T.append(np.matmul(np.matmul(np.diag(np.transpose(alpha)), np.transpose(M)),np.transpose(one)) + beta)
-    rms = sqrt(mean_squared_error(E_T[i], E_T[i+1]))
-    if rms == 0:
-        print("CONVERGED")
+amplification_post = [] # for each shock this stores the amplification score for plotting
+amplification_pre = []
+for i in range(len(df_2000.columns)):
+    M_T = []
+    M_T.append(M)
+    for j in range(len(M)):
+        if i==j:
+            M[j] = 0
+    M_1 = [] #Depending on the kind of shock you have to populate this and pass it into M_T, calculate E1 and pass it to E_T and then iterate with the below for loop
+    E_T = []
+    E_T.append(E)
+    for i in range(10):
+        M_T.append(np.matmul(np.diag(E_T[i]),m))
+        E_T.append(np.matmul(np.matmul(np.diag(np.transpose(alpha)), np.transpose(M)),np.transpose(one)) + beta)
+        rms = sqrt(mean_squared_error(E_T[i], E_T[i+1]))
+        if rms == 0:
+            print("CONVERGED")
+
+    expenditure_difference_post = E_T[len(E_T)] - E_T[1] #in comparison after initial shock has been applied
+    expenditure_difference_pre = E_T[len(E_T)] - E_T[0] #in comparison before initial shock has been applied
+    amplification_post.append(sum(expenditure_difference_post))
+    amplification_pre.append(sum(expenditure_difference_pre))
+
+# Plotting the amplification of removing each node
+plt.subplot(2, 1, 1)
+plt.plot(df_2000.columns, amplification_post, 'o-')
+plt.title('Amplification VS Node Deletion')
+plt.ylabel('Amplification')
+
+plt.subplot(2, 1, 2)
+plt.plot(df_2000.columns, amplification_pre, '.-')
+plt.xlabel('Industries')
+plt.ylabel('Amplification')
+
+plt.show()
